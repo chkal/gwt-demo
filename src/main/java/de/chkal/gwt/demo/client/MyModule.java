e7bb49f7a4258c504bb02fcbe48b0e6e01f9408b
Christian Kaltepoth <christian@kaltepoth.de>
2012-08-07 07:29:08 +0200

    Initial

---
+++ src/main/java/de/chkal/gwt/demo/client/MyModule.java
@@ -0,0 +1,15 @@
+package de.chkal.gwt.demo.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+public class MyModule implements EntryPoint
+{
+
+   @Override
+   public void onModuleLoad()
+   {
+      Window.alert("Hello world, GWT rocks!");
+   }
+
+}
